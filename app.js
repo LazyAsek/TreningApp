846d3a2eead422930b668571feb946fc33a746b1
LazyAsek <jwlochwa@gmail.com>
2022-06-22 23:28:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 const dayArray = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
-const MondayWorkout = ["pompka","dimond push ups","wybuchowa","wolna pompka","pompka","diamentowo->normalna" ,"pompka z podnoszeniem ronk","smocza pompka","archer pushups","pompka"]
+const WednesdayWorkout = ["pompka","dimond push ups","wybuchowa","wolna pompka","pompka","diamentowo->normalna" ,"pompka z podnoszeniem ronk","smocza pompka","archer pushups","pompka incline","pompka"]
 
-const TuesdayWorkout = ["podciaganie recznik","buterfly","2kg hantel","podciagani normalne","podciaganie daleko","podciaganie blisko","podciaganie neutralny","podchwyt klata do drazka","3kg hantel","ironmen","pompki opadajace","angel z butlami","4kg","wiszenie"]
+const TuesdayWorkout = ["podciaganie recznik","buterfly","2kg hantel","podciagani normalne bez nog","podciaganie daleko","3kg hantel","podciaganie blisko","podciaganie neutralny bez nog pol","podchwyt klata do drazka","3kg hantel","ironmen","pompki opadajace","angel z butlami","4kg","wiszenie"]
 
-const WednesdayWorkout = [ "brzuszki",  "wolne brzuszki", "siedzace brzuszki", "side boxing brzuszki", "podnoszenie kolan","podnoszenie nog", "rowerek", "norzyce","v hold","brzuszki kolanowe renka do kolana na bok","deska na boku na bok", "norzyce z podnoszeniem nog","pompka podnoszenie nog","wiszenie podnoszenie nog" ,"wiszenie podnoszenie nog"]
+const MondayWorkout = [ "brzuszki",  "wolne brzuszki", "siedzace brzuszki", "side boxing brzuszki", "podnoszenie kolan","podnoszenie nog", "rowerek", "norzyce","v hold","brzuszki kolanowe renka do kolana na bok","deska na boku na bok", "norzyce z podnoszeniem nog","pompka podnoszenie nog","wiszenie podnoszenie nog" ,"wiszenie podnoszenie nog"]
 
 const ThursdayWorkout = ["przysiady","przysiady z wyskokiem","przysiad  czekaniem 5s","podnoszenie 1 nogi(lezenie)","przysiad 1 noga","bulgarian squad skok","opieranie sie o sciane  plecy do sciany","stawanie na palcach ","podwyzszenie hary pota","stawwanie na palcah z hantlem 5 kg"]
 
-const FridayWorkout = ["rowerowa pompka","rowerowa pompka z czekanie","rowerowa pompka z poruszniem","pompka wysuwanie do przodu","side shoulder rises(podnosznie dupy na boku deski","bombowapompka ","wolna rowerowa pompka","pompka z podnoszeniem butealki na bok (raz na ramie)","dotykanie ramion z podwyzszenia","stanie na podwyzszeniu 60","handstand"]
+const FridayWorkout = ["rowerowa pompka","rowerowa pompka z czekanie","rowerowa pompka z poruszniem","pompka wysuwanie do przodu","side shoulder rises(podnosznie dupy na boku deski","bombowapompka ","wolna rowerowa pompka","pompka z podnoszeniem butealki na bok (raz na ramie)","dotykanie ramion z podwyzszenia","stanie na podwyzszeniu 60","hantel na bok 3kg"]
 
 let currentDay =0
 let currentDayName = document.querySelector(".day")
@@ -19,124 +19,18 @@ function changeDayRight(){
 currentDay +=1
 if(currentDay>6){currentDay=0}
 currentDayName.innerHTML = dayArray[currentDay]
+Workouts()
 }
 
 function changeDayLeft(){
     currentDay -=1
     if(currentDay<0){currentDay=6}
     currentDayName.innerHTML = dayArray[currentDay]
+    Workouts()
     }
 
 function setsMonday(a){
 
-if(a == 0){
-    workoutDay.innerHTML += " 40 "
-    checkbox(1)
-}
-
-if(a == 1){
-    workoutDay.innerHTML += " 3x10 "
-    checkbox(3)
-}
-
-if(a == 2){
-    workoutDay.innerHTML += " 3x10 "
-    checkbox(3)
-    }
-if(a == 3){
-    workoutDay.innerHTML += " 1x10 "
-    checkbox(1)
-}
-if(a == 4){
-    workoutDay.innerHTML += " 30s "
-    checkbox(1)
-    }
-if(a == 5){
-    workoutDay.innerHTML += " 2x10 "
-    checkbox(2)
-    }
-if(a == 6){
-    workoutDay.innerHTML += " 2x10 "
-    checkbox(2)
-    }
-if(a == 7){
-    workoutDay.innerHTML += " 2x10 "
-    checkbox(2)
-    }
-if(a == 8){
-    workoutDay.innerHTML += " 2x10 "
-    checkbox(2)
-    }
- if(a == 9){
-    workoutDay.innerHTML += " 10x10 "
-    checkbox(10)
-    }
-}
-
-function setsTuesday(a){
-
-    if(a == 0){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-    }
-    
-    if(a == 1){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-    }
-    
-    if(a == 2){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-        }
-    if(a == 3){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-    }
-    if(a == 4){
-        workoutDay.innerHTML += " 1x10 "
-        checkbox(1)
-        }
-    if(a == 5){
-        workoutDay.innerHTML += " 1x10 "
-        checkbox(1)
-        }
-    if(a == 6){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-        }
-    if(a == 7){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-        }
-    if(a == 8){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-        }
-    if(a == 9){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-    }
-    if(a == 10){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
-        }
-    if(a == 11){
-         workoutDay.innerHTML += " 2x10 "
-         checkbox(2)
-         }
-    if(a == 12){
-         workoutDay.innerHTML += " 2x10 "
-          checkbox(2)
-          }
-    if(a == 13){
-        workoutDay.innerHTML += " 2x30s "
-         checkbox(2)
-         }
-    }
-    
-function setsWednesday(a){
-
     if(a == 0){
         workoutDay.innerHTML += " 20 "
             checkbox(1)
@@ -172,7 +66,7 @@ function setsWednesday(a){
             checkbox(1)
             }
     if(a == 8){
-        workoutDay.innerHTML += " 30s "
+        workoutDay.innerHTML += " 60s "
             checkbox(1)
             }
     if(a == 9){
@@ -200,6 +94,123 @@ function setsWednesday(a){
          checkbox(2)
          }
 }
+
+
+function setsTuesday(a){
+
+    if(a == 0){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+    }
+    
+    if(a == 1){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+    }
+    
+    if(a == 2){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 3){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+    }
+    if(a == 4){
+        workoutDay.innerHTML += " 1x10 "
+        checkbox(1)
+        }
+    if(a == 5){
+         workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+          }
+    if(a == 6){
+        workoutDay.innerHTML += " 1x10 "
+        checkbox(1)
+        }
+    if(a == 7){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 8){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 9){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 10){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+    }
+    if(a == 11){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 12){
+         workoutDay.innerHTML += " 2x10 "
+         checkbox(2)
+         }
+    if(a == 13){
+         workoutDay.innerHTML += " 2x10 "
+          checkbox(2)
+          }
+    if(a == 14){
+        workoutDay.innerHTML += " 2x30s "
+         checkbox(2)
+         }
+    }
+    
+function setsWednesday(a){
+    if(a == 0){
+        workoutDay.innerHTML += " 40 "
+        checkbox(1)
+    }
+    
+    if(a == 1){
+        workoutDay.innerHTML += " 3x10 "
+        checkbox(3)
+    }
+    
+    if(a == 2){
+        workoutDay.innerHTML += " 3x10 "
+        checkbox(3)
+        }
+    if(a == 3){
+        workoutDay.innerHTML += " 1x10 "
+        checkbox(1)
+    }
+    if(a == 4){
+        workoutDay.innerHTML += " 30s "
+        checkbox(1)
+        }
+    if(a == 5){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 6){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 7){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+    if(a == 8){
+        workoutDay.innerHTML += " 2x10 "
+        checkbox(2)
+        }
+     if(a == 9){
+        workoutDay.innerHTML += " 1x10 "
+        checkbox(1)
+        }
+        if(a == 10){
+            workoutDay.innerHTML += " 5x10 "
+            checkbox(5)
+            }
+    }
+    
     
 function setsThursday(a){
 
@@ -214,8 +225,8 @@ function setsThursday(a){
     }
     
     if(a == 2){
-        workoutDay.innerHTML += " 2x10 "
-        checkbox(2)
+        workoutDay.innerHTML += " 3x10 "
+        checkbox(3)
         }
     if(a == 3){
         workoutDay.innerHTML += " 2x10 "
@@ -230,7 +241,7 @@ function setsThursday(a){
         checkbox(2)
         }
     if(a == 6){
-        workoutDay.innerHTML += " 30s "
+        workoutDay.innerHTML += " 60s "
         checkbox(1)
         }
     if(a == 7){
@@ -280,34 +291,36 @@ function setsFriday(a){
             checkbox(1)
             }
     if(a == 7){
-            workoutDay.innerHTML += " 1x10 "
-            checkbox(1)
+            workoutDay.innerHTML += " 2x10 "
+            checkbox(2)
             }
     if(a == 8){
-            workoutDay.innerHTML += " 40s "
+            workoutDay.innerHTML += " 40 "
             checkbox(1)
             }
     if(a == 9){
-             workoutDay.innerHTML += " 40s "
+             workoutDay.innerHTML += "s "
              checkbox(1)
              }
-    if(a == 10){
-             workoutDay.innerHTML += " 30s "
-             checkbox(1)
+             if(a == 10){
+                workoutDay.innerHTML += " 2 x 10 na ramie "
+                checkbox(1)
                 }
+
         }
 
 
 function checkbox(number){
     for(b=0; b<number ; b++){
-    workoutDay.innerHTML +="<input type = 'checkbox' id='box'>"
+       const box = document.createElement("input")
+    workoutDay.innerHTML +="<input type ='checkbox' id='box'>"
     }
 }
 
 
 
 Workouts()
-document.addEventListener("click",Workouts)
+
 function Workouts(){
 
     const margin = document.createElement("div")
